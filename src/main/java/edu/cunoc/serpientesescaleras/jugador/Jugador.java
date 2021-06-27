fedabf4e225df2cb5d3ab054b56c85c80cb6da32
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-06-27 15:39:40 -0600

    DEFINIDA LA CLASE Jugador.java

--- src/main/java/edu/cunoc/serpientesescaleras/jugador/Jugador.java
+++ src/main/java/edu/cunoc/serpientesescaleras/jugador/Jugador.java
@@ -7,11 +7,102 @@ public class Jugador {
     private int partidasJugadas;
     private int partidasGanadas;
     private int partidasPerdidas;
+    private int posicionAhora;
+    private int posicionAntes;
+    private Object colorFicha;
+    private int pierdeTurno;
+
+    public Jugador() {
+        //jugador en sus inicios
+        this.partidasJugadas = 0;
+        this.partidasGanadas = 0;
+        this.posicionAhora = 1;
+        this.pierdeTurno = 0;
+    }
     
+    //metodos
     
+    //set get
+    public int getPierdeTurno() {
+        return pierdeTurno;
+    }
+
+    public void setPierdeTurno(int pierdeTurno) {
+        this.pierdeTurno = pierdeTurno;
+    }
     
-    
-    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreJugador() {
+        return nombreJugador;
+    }
+
+    public void setNombreJugador(String nombreJugador) {
+        this.nombreJugador = nombreJugador;
+    }
+
+    public String getApellidoJugador() {
+        return apellidoJugador;
+    }
+
+    public void setApellidoJugador(String apellidoJugador) {
+        this.apellidoJugador = apellidoJugador;
+    }
+
+    public int getPartidasJugadas() {
+        return partidasJugadas;
+    }
+
+    public void setPartidasJugadas(int partidasJugadas) {
+        this.partidasJugadas = partidasJugadas;
+    }
+
+    public int getPartidasGanadas() {
+        return partidasGanadas;
+    }
+
+    public void setPartidasGanadas(int partidasGanadas) {
+        this.partidasGanadas = partidasGanadas;
+    }
+
+    public int getPartidasPerdidas() {
+        return partidasPerdidas;
+    }
+
+    public void setPartidasPerdidas(int partidasPerdidas) {
+        this.partidasPerdidas = partidasPerdidas;
+    }
+
+    public int getPosicionAhora() {
+        return posicionAhora;
+    }
+
+    public void setPosicionAhora(int posicionAhora) {
+        this.posicionAhora = posicionAhora;
+    }
+
+    public int getPosicionAntes() {
+        return posicionAntes;
+    }
+
+    public void setPosicionAntes(int posicionAntes) {
+        this.posicionAntes = posicionAntes;
+    }
+
+    public Object getColorFicha() {
+        return colorFicha;
+    }
+
+    public void setColorFicha(Object colorFicha) {
+        this.colorFicha = colorFicha;
+    }
+
     
     
 }
