0acae073c7859364656e63af98cae90abf010b7d
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-06-27 15:30:11 -0600

    COMENCE A TRABAJAR EL JUGADOR, DEFINIENDO ATRIBUTOS Y METODOS

---
+++ src/main/java/edu/cunoc/serpientesescaleras/jugador/Jugador.java
@@ -0,0 +1,17 @@
+package edu.cunoc.serpientesescaleras.jugador;
+
+public class Jugador {
+    private int id;
+    private String nombreJugador;
+    private String apellidoJugador;
+    private int partidasJugadas;
+    private int partidasGanadas;
+    private int partidasPerdidas;
+    
+    
+    
+    
+    
+    
+    
+}
