ccbfefbde383a5e362a75a86619dad72effaf1d8
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-06-27 00:40:34 -0600

    SE AGREGARON IMAGENEX

--- src/main/java/edu/cunoc/serpientesescaleras/principla/Bienvenida.java
+++ src/main/java/edu/cunoc/serpientesescaleras/principla/Bienvenida.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.cunoc.serpientesescaleras.principla;
 
-/**
- *
- * @author orcha
- */
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 public class Bienvenida {
+    //variables
+    private Object jugadores;
+    private Object espacios;
+    
+    //constructor
+
+    public Bienvenida() {
+        ImageIcon f1 = new ImageIcon(getClass().getResource("inicio.jpg"));
+        JLabel bienv = new JLabel(f1);
+        bienv.setSize(1000, 1000);
+        bienv.setVisible(true);
+        
+
+        
+    }
+    
+    
+    
+    
+    
+    
     
 }
