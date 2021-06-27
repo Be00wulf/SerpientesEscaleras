66f09846c76a481ba6b0dab7f6019e4975c57dc1
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-06-27 15:18:56 -0600

    SE AGREGO LA BIENVENIDA

--- src/main/java/edu/cunoc/serpientesescaleras/principla/Bienvenida.java
+++ src/main/java/edu/cunoc/serpientesescaleras/principla/Bienvenida.java
@@ -16,15 +16,25 @@ public class Bienvenida {
         JLabel bienv = new JLabel(f1);
         bienv.setSize(1000, 1000);
         bienv.setVisible(true);
-        
-
-        
+        ingresarDatos();
     }
     
+    //metodos
+    public void ingresarDatos(){
+    //dimensiones del tablero
+    (this.espacios) = JOptionPane.showInputDialog(null, "Seleccione el numero de Casillas",   "" + "Casillas",   JOptionPane.QUESTION_MESSAGE,null, new Object[] {16,25,36,49},64);
     
+    //cantidad de jugadores
+     jugadores = JOptionPane.showInputDialog(null, "Seleccione el numero de jugadores", "Jugadores",   JOptionPane.QUESTION_MESSAGE,null, new Object[] {2,3,4,5,6},   "");
+    }
     
-    
-    
-    
-    
+    //get set
+    public Object getJugadores() {
+        return jugadores;
+    }
+
+    public Object getEspacios() {
+        return espacios;
+    }
+
 }
