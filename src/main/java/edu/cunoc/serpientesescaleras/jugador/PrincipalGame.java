7d321c67ad7068311a4e0668df8e27c55ad551b6
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-06-28 01:27:59 -0600

    PrincipalGame.java modificado, revisar error de (int)espacios

--- src/main/java/edu/cunoc/serpientesescaleras/jugador/PrincipalGame.java
+++ src/main/java/edu/cunoc/serpientesescaleras/jugador/PrincipalGame.java
@@ -1,6 +1,7 @@
 package edu.cunoc.serpientesescaleras.jugador;
 
 import java.awt.Color;
+import java.util.Random;
 import javax.swing.JColorChooser;
 import javax.swing.JOptionPane;
 
@@ -26,6 +27,40 @@ public class PrincipalGame {
             j1[i].setNombreJugador(nombre);
             j1[i].setColorFicha(colorFicha); 
         }
+        
+        //casillas normales
+        Casilla[] c1;
+        c1 = new Casilla[(int)espacios + 1];
+        
+        //casilla simple
+        for (int i = 1; i<=(int)espacios;i++){
+            c1[i] = new Casilla();
+            c1[i].setClaseCasilla("NORMAL");
+        }
+        
+        //CASILLA DE PIERDE TURNO
+        Random aleatorio = new Random();
+        for (int i = 1; i <=5 ;i++){
+            int varAleatoria = aleatorio.nextInt((int)(espacios))+1;
+            while(true){
+                if (varAleatoria == 1 || varAleatoria == (int)espacios){                                                                //REVISAR
+                    varAleatoria = aleatorio.nextInt((int)(espacios))+1;
+                    continue;
+                }
+                break;//se sale del ciclo
+            }
+            c1[varAleatoria].setClaseCasilla("PERDER TURNO");
+       }
+        
+       //casillas de cambiar el tablero 
+        
+        
+        
+        
+        
+        
+        
+        
         
         
     }
